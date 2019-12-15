f3e1bd923eb6007e3fc4069cfe56c7be119244fb
David Sabalete Rodríguez <david.sabalete@lovetoknow.com>
2019-12-15 03:35:25 +0100

    fix gridsome config

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,15 +1,19 @@
 module.exports = {
-  siteName: 'Gridsome starter resume',
-  siteDescription: 'A starter project for Gridsome with Bootstrap for you to quickly build a resume.',
-  siteUrl: 'https://gridsome-starter-resume.loke.dev',
-  plugins: [{
-      use: '@gridsome/plugin-google-analytics',
+  siteName: "Gridsome starter resume",
+  siteDescription:
+    "A starter project for Gridsome with Bootstrap for you to quickly build a resume.",
+  siteUrl: "https://gridsome-starter-resume.loke.dev",
+  siteUrl: "https://davidsabaleteltk.github.io",
+  pathPrefix: "/gridsome-dsr",
+  plugins: [
+    {
+      use: "@gridsome/plugin-google-analytics",
       options: {
-        id: 'UA-72659574-11'
+        id: "UA-72659574-11"
       }
     },
     {
-      use: '@gridsome/plugin-sitemap',
+      use: "@gridsome/plugin-sitemap",
       options: {
         cacheTime: 600000
       }
@@ -20,4 +24,4 @@ module.exports = {
       scss: {}
     }
   }
-}
+};
