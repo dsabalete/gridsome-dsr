89b9c8ffffb51e94a04d52fb872cf3eccc925ad0
David Sabalete Rodríguez <david.sabalete@lovetoknow.com>
2020-12-30 21:18:51 +0100

    update google analytics id

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  siteName: "Gridsome starter resume",
-  siteDescription:
-    "A starter project for Gridsome with Bootstrap for you to quickly build a resume.",
-  siteUrl: "https://davidsabaleteltk.github.io",
-  // pathPrefix: "/davidsabaleteLTK.github.io",
-  plugins: [
-    {
-      use: "@gridsome/plugin-google-analytics",
-      options: {
-        id: "UA-72659574-11"
-      }
-    },
-    {
-      use: "@gridsome/plugin-sitemap",
-      options: {
-        cacheTime: 600000
-      }
+    siteName: 'Gridsome starter resume',
+    siteDescription:
+        'A starter project for Gridsome with Bootstrap for you to quickly build a resume.',
+    siteUrl: 'https://dsabalete.github.io',
+    // pathPrefix: "/davidsabaleteLTK.github.io",
+    plugins: [
+        {
+            use: '@gridsome/plugin-google-analytics',
+            options: {
+                id: 'UA-1969234-7'
+            }
+        },
+        {
+            use: '@gridsome/plugin-sitemap',
+            options: {
+                cacheTime: 600000
+            }
+        }
+    ],
+    css: {
+        loaderOptions: {
+            scss: {}
+        }
     }
-  ],
-  css: {
-    loaderOptions: {
-      scss: {}
-    }
-  }
-};
+}
