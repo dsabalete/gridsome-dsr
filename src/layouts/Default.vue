bbdd7cd57c518d39758a2ac776484a2a5e6a1b06
David Sabalete Rodríguez <david.sabalete@lovetoknow.com>
2021-01-13 23:26:47 +0100

    fix: increase font size for user a11y

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="layout">
-    <Nav />
-    <slot />
-  </div>
+    <div class="layout">
+        <Nav />
+        <slot />
+    </div>
 </template>
 
 <script>
-import Nav from "../components/Nav";
+import Nav from '../components/Nav'
 
 export default {
-  components: {
-    Nav
-  }
-};
+    components: {
+        Nav
+    }
+}
 </script>
 
-
 <style lang="scss">
+.layout {
+    font-size: 1.1rem;
+}
 </style>
